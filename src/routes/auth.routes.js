9dd712a97b62aff0972c7ae2501a4f37de092e54
allooshx <lamr92246@gmail.com>
2026-07-27 23:06:23 +0300

    Fix user route path

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,36 +11,61 @@ const {
 } = require("../controllers/auth.controller");
 
 
+const {
+    registerValidator,
+    loginValidator,
+    forgotPasswordValidator,
+    resetPasswordValidator
+} = require("../validators/auth.validator");
+
+
+const validate = require("../middleware/validation.middleware");
+
+
+// Register
 router.post(
     "/register",
+    registerValidator,
+    validate,
     register
 );
 
+
+// Login
 router.post(
     "/login",
+    loginValidator,
+    validate,
     login
 );
+
+
+// Forgot Password
 router.post(
     "/forgot-password",
+    forgotPasswordValidator,
+    validate,
     forgotPassword
 );
 
 
+// Reset Password
 router.post(
     "/reset-password",
+    resetPasswordValidator,
+    validate,
     resetPassword
 );
 
-const {
-    googleLogin
-}
-    =
-    require("../controllers/social.controller");
+
+// Google Login
+const { googleLogin } = require("../controllers/social.controller");
 
 
 router.post(
     "/google",
     googleLogin
 );
+
 
 module.exports = router;
