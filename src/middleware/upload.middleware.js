a7287b6e218d71c35bfc9cbb5fe4177e50fc7195
allooshx <lamr92246@gmail.com>
2026-07-29 05:14:22 +0300

    fix ai plant route

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,33 +1,35 @@
 const multer = require("multer");
+const fs = require("fs");
+
+
+if (!fs.existsSync("uploads")) {
+
+    fs.mkdirSync("uploads");
+
+}
 
 
 const storage = multer.diskStorage({
 
-    destination: (req, file, cb) => {
+    destination:(req,file,cb)=>{
 
-        cb(null, "uploads/");
+        cb(null,"uploads/");
 
     },
 
 
-    filename: (req, file, cb) => {
+    filename:(req,file,cb)=>{
 
-        const uniqueName =
-            Date.now() + "-" + file.originalname;
-
-
-        cb(null, uniqueName);
+        cb(
+            null,
+            Date.now() + "-" + file.originalname
+        );
 
     }
 
 });
 
 
-const upload = multer({
-
+module.exports = multer({
     storage
-
 });
-
-
-module.exports = upload;
