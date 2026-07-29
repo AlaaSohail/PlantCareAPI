86b66a3ee42a077ed9615f8631ea1cbb2271417e
allooshx <lamr92246@gmail.com>
2026-07-29 07:27:31 +0300

    fix reset email link

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -7,38 +7,45 @@ const resend = new Resend(
 
 
 
-const sendResetEmail = async (
-    email,
-    token
-) => {
+const sendResetEmail = async (email, token) => {
 
 
-    const resetUrl =
-        `https://your-frontend.com/reset-password?token=${token}`;
+    const resetLink =
+        `https://alaasohail.com/reset-password?token=${token}`;
 
 
 
-    await resend.emails.send({
+    const result =
+        await resend.emails.send({
 
-        from: "PlantCare <noreply@alaasohail.com>",
+            from:
+            "PlantCare <noreply@alaasohail.com>",
 
-        to: email,
+            to: email,
 
-        subject: "Reset your PlantCare password",
+            subject:
+            "Reset your PlantCare password",
 
-        html: `
-        <h2>PlantCare</h2>
+            html: `
 
-        <p>
-        Click below to reset your password:
-        </p>
+            <h2>PlantCare</h2>
 
-        <a href="${resetLink}">
-            Reset Password
-        </a>
-    `
+            <p>
+            You requested to reset your password.
+            </p>
 
-    });
+
+            <a href="${resetLink}">
+                Reset Password
+            </a>
+
+
+            `
+
+        });
+
+
+    console.log(result);
 
 
 };
