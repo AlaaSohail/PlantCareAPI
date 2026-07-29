46d48cabe604bdd19c1d92319326b14f123cd7b9
allooshx <lamr92246@gmail.com>
2026-07-29 03:29:13 +0300

    fix database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,17 @@
 const { Pool } = require("pg");
 
+console.log("LOADING DATABASE FILE");
+console.log("DATABASE URL =", process.env.DATABASE_URL);
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
+});
+
+pool.on("error", (err) => {
+    console.log("POOL ERROR:", err);
 });
 
 module.exports = pool;
